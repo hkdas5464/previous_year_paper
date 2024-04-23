2c4a9431e06f642f1550fdbbd53470355af9f036
hkdas5464 <hkdas5464@gmail.com>
2024-04-23 20:00:32 +0530

    third commit

--- src/app/ssc_cgl/page.tsx
+++ src/app/ssc_cgl/page.tsx
@@ -11,7 +11,7 @@ import  {CurrentAffairs, EnglishData, Mathsdata}  from "./data";
 function page() {
   return (
     <div className="max-w-4xl mx-auto pb-40 bg-black">
-      <p className="text-2xl mt-20 md:text-4xl lg:text-7xl text-white font-bold inter-var text-center">
+      <p className="text-2xl mt-20 md:text-4xl mb-5 lg:text-7xl text-white font-bold inter-var text-center">
         SSC CGL
       </p>
       
