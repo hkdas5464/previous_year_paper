1594112776fa868f62aeee6e5260d3ce01e703c4
hkdas5464 <hkdas5464@gmail.com>
2024-05-22 17:41:02 +0530

    parmarpyq commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -145,7 +145,32 @@ export default function MyHome() {
             <Tab key="notes" title="Notes">
               <Card>
                 <CardBody>
-{/* <Home/> */}
+                <div className="mb-32 grid text-center lg:mb-0 lg:w-full lg:max-w-6xl lg:grid-cols-5 lg:text-left">
+                    <Link
+                      href={"parmar"}
+                      className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
+
+                      rel="noopener noreferrer"
+                    >
+                      <h2 className="mb-3 text-2xl font-semibold">
+                        Parmar_SSC{" "}
+                        <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
+                          -&gt;
+                        </span>
+                      </h2>
+                      <p className="m-0 max-w-[30ch] text-sm opacity-50">
+                        Find in-depth information about Next.js features and API.
+                      </p>
+                    </Link>
+
+
+               
+
+                  
+
+                    
+                   
+                  </div>
                 </CardBody>
               </Card>
             </Tab>
