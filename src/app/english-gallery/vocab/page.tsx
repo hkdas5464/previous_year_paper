1bfaf1476c30234e65e4fe95fdc13cbb93484b4f
hkdas5464 <hkdas5464@gmail.com>
2024-06-14 16:40:11 +0530

    check box

--- src/app/english-gallery/vocab/page.tsx
+++ src/app/english-gallery/vocab/page.tsx
@@ -1,6 +1,6 @@
 'use client'
 import React from "react";
-import { Tabs, Tab, Card, CardBody, CardHeader } from "@nextui-org/react";
+import { Tabs, Tab, Card, CardBody, CardHeader, Checkbox } from "@nextui-org/react";
 import { vocab_day_1, vocab_day_2, vocab_day_3 } from './data'
 import './app.css'
 
@@ -25,17 +25,18 @@ export default function App() {
                     <Tab key="GK" title="Day_1">
 
                         <Card className="gap-2 grid grid-cols-2 sm:grid-cols-4">
-                            {vocab_day_1.map((e,index) => (
+                            {vocab_day_1.map((e, index) => (
                                 <CardBody key={Math.random()} className="overflow-visible p-0">
                                     <div className="flip-card">
                                         <div className="flip-card-inner">
                                             <div className="flip-card-front">
+                                            <p>{1 + index}</p>
                                                 <p className="title">{e.voc}</p>
-                                                <p>{1+index}</p>
-
+                                                
                                             </div>
                                             <div className="flip-card-back">
                                                 <p className="title">{e.meaning}</p>
+                                                <p><Checkbox /></p>
 
                                             </div>
                                         </div>
@@ -48,13 +49,13 @@ export default function App() {
                     <Tab key="current" title="Day_2">
 
                         <Card className="gap-2 grid grid-cols-2 sm:grid-cols-4">
-                            {vocab_day_2.map((e,index) => (
+                            {vocab_day_2.map((e, index) => (
                                 <CardBody key={Math.random()} className="overflow-visible p-0">
                                     <div className="flip-card">
                                         <div className="flip-card-inner">
                                             <div className="flip-card-front">
                                                 <p className="title">{e.voc}</p>
-                                                <p>{1+index}</p>
+                                                <p>{1 + index}</p>
 
                                             </div>
                                             <div className="flip-card-back">
@@ -71,13 +72,13 @@ export default function App() {
                     <Tab key="Day_3" title="Day_3">
 
                         <Card className="gap-2 grid grid-cols-2 sm:grid-cols-4">
-                            {vocab_day_3.map((e,index) => (
+                            {vocab_day_3.map((e, index) => (
                                 <CardBody key={Math.random()} className="overflow-visible p-0">
                                     <div className="flip-card">
                                         <div className="flip-card-inner">
                                             <div className="flip-card-front">
                                                 <p className="title">{e.voc}</p>
-<p>{1+index}</p>
+                                                <p>{1 + index}</p>
                                             </div>
                                             <div className="flip-card-back">
                                                 <p className="title">{e.meaning}</p>
@@ -93,13 +94,13 @@ export default function App() {
                     <Tab key="Day_4" title="Day_4">
 
                         <Card className="gap-2 grid grid-cols-2 sm:grid-cols-4">
-                            {vocab_day_3.map((e,index) => (
-                                 <CardBody key={Math.random()} className="overflow-visible p-0">
+                            {vocab_day_3.map((e, index) => (
+                                <CardBody key={Math.random()} className="overflow-visible p-0">
                                     <div className="flip-card">
                                         <div className="flip-card-inner">
                                             <div className="flip-card-front">
                                                 <p className="title">{e.voc}</p>
-<p>{1+index}</p>
+                                                <p>{1 + index}</p>
                                             </div>
                                             <div className="flip-card-back">
                                                 <p className="title">{e.meaning}</p>
@@ -115,13 +116,13 @@ export default function App() {
                     <Tab key="Day_5" title="Day_5">
 
                         <Card className="gap-2 grid grid-cols-2 sm:grid-cols-4">
-                            {vocab_day_3.map((e,index) => (
-                                  <CardBody key={Math.random()} className="overflow-visible p-0">
+                            {vocab_day_3.map((e, index) => (
+                                <CardBody key={Math.random()} className="overflow-visible p-0">
                                     <div className="flip-card">
                                         <div className="flip-card-inner">
                                             <div className="flip-card-front">
                                                 <p className="title">{e.voc}</p>
-<p>{index+1}</p>
+                                                <p>{index + 1}</p>
                                             </div>
                                             <div className="flip-card-back">
                                                 <p className="title">{e.meaning}</p>
