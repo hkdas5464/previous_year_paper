09846b65a44e4e14de4571e24d08107cef049dba
hkdas5464 <hkdas5464@gmail.com>
2024-06-21 14:36:52 +0530

    id

--- src/app/videos/maths/Cards.tsx
+++ src/app/videos/maths/Cards.tsx
@@ -8,48 +8,56 @@ export default function CardApp() {
       title: "Orange",
       img: "https://cdn.pixabay.com/photo/2021/11/10/18/44/mathematics-6784603_1280.jpg",
       price: "$5.50",
+      id:1,
     },
     {
       title: "Tangerine",
       img: "/images/fruit-2.jpeg",
       price: "$3.00",
+      id:2,
     },
     {
       title: "Raspberry",
       img: "/images/fruit-3.jpeg",
       price: "$10.00",
+      id:3,
     },
     {
       title: "Lemon",
       img: "/images/fruit-4.jpeg",
       price: "$5.30",
+      id:4,
     },
     {
       title: "Avocado",
       img: "/images/fruit-5.jpeg",
       price: "$15.70",
+      id:5,
     },
     {
       title: "Lemon 2",
       img: "/images/fruit-6.jpeg",
       price: "$8.00",
+      id:6,
     },
     {
       title: "Banana",
       img: "/images/fruit-7.jpeg",
       price: "$7.50",
+      id:7,
     },
     {
       title: "Watermelon",
       img: "/images/fruit-8.jpeg",
       price: "$12.20",
+      id:8,
     },
   ];
 
   return (
     <div className="gap-2 grid grid-cols-2 sm:grid-cols-4">
-      {list.map((item, index) => (
-       <Link href={'youtube'}> 
+      {list.map((item) => (
+       <Link href={'youtube'} key={item.id} > 
        <Card shadow="sm" key={Math.random()} >
           <CardBody key={Math.random()} className="overflow-visible p-0">
             <Image
