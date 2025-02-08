2163ceef0542d7d80ea910ffe6e59261af974a13
Your Name <hkdas5464@gmail.com>
2025-02-08 13:28:03 +0530

    added homepage link

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -78,6 +78,22 @@ export default function MyHome() {
           - Blackbook
         </p>
       </Link>
+
+      <Link
+        href={"learning/gs"}
+        className="group rounded-lg border  border-transparent px-5 py-4 transition-colors hover:text-blue-900 hover:border-blue-600 hover:bg-white hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
+        rel="noopener noreferrer"
+      >
+        <h2 className="mb-3 text-2xl font-semibold">
+          All GS{" "}
+          <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
+            -&gt;
+          </span>
+        </h2>
+        <p className="m-0 max-w-[30ch] text-sm opacity-50">
+          
+        </p>
+      </Link>
     </div>
   </CardBody>
 </Card>
